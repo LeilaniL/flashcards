868135e18a775744b9bc0ade9e33cf7bedf33fda
Leilani Leach and Stuart Gill <leilani@>
2018-10-16 13:33:26 -0700

    show and hide terms and definitions correctly

--- js/scripts.js
+++ js/scripts.js
@@ -3,21 +3,27 @@ $(document).ready(function() {
     return 't' + (n+1);
   });
 
-  $("h2").click(function() {
+  $(".term h2").click(function() {
     var id = $(this).parent().attr("id");
     var destination = "#" + id + " p";
-    $(".definition").show();
-    $(".term").hide();
+    var destination2 = "#" + id + " h2";
+    $(destination).show("200ms");
+    $(destination2).hide("200ms");
   });
 
-  $("p").click(function() {
+  $(".term p").click(function() {
     var id = $(this).parent().attr("id");
-    var destination = "#" + id + " p";
-    $(".term").show("200ms");
-    $(".definition").hide();
+    var destination = "#" + id + " h2";
+    var destination2 = "#" + id + " p";
+    $(destination).show("200ms");
+    $(destination2).hide("200ms");
   });
 
-  $('div.term').attr('id', function(n) {
-    return 't' + (n+1);
-  });
+  // $("p").click(function() {
+  //   var id = $(this).parent().attr("id");
+  //   var destination = "#" + id + " p";
+  //   $(".term").show("200ms");
+  //   $(".definition").hide();
+  // });
+
 });
